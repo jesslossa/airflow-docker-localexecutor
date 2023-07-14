5ae41fc56228ef8dac31714b7bcfdb81c5c862b5
Jessica Lossavaro Silva <jessica.lossavaro@semantix.ai>
2023-07-14 11:57:17 -0300

    Added dag with python parameters

---
+++ dags/dag_python_operator_basic.py
@@ -0,0 +1,27 @@
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+
+
+default_args = {
+    'owner': 'Jessica',
+    'retries': 5,
+    'retry_delay': timedelta(minutes=5)
+}
+
+def greet():
+    print("Hey, Basic Hello World!")
+
+
+with DAG(
+    default_args=default_args,
+    dag_id='dag_python_operator_basic',
+    description='Basic dag using PythonOperator',
+    start_date=datetime(2023, 7, 10),
+    schedule_interval='@daily'
+) as dag:
+    task1 = PythonOperator(
+        task_id='greet',
+        python_callable=greet
+    )
