bfff825785ab415742d621e2e56d989a0a0b582e
Jessica Lossavaro Silva <jessica.lossavaro@semantix.ai>
2023-07-14 12:20:07 -0300

    Added dag with first and last name xcom parameters

--- dags/dag_python_operator_xcom_first-last-name.py
+++ dags/dag_python_operator_xcom_first-last-name.py
@@ -10,17 +10,19 @@ default_args = {
     'retry_delay': timedelta(minutes=5)
 }
 
-def greet(name, age):
-    print(f"Hello World from {name}! I am {age} years old.")
+def greet(age, ti):
+    first_name = ti.xcom_pull(task_ids='get_name', key ='first_name')
+    last_name = ti.xcom_pull(task_ids='get_name', key ='last_name')
+    print(f"Hello World from {last_name}, {first_name}! I am {age} years old.")
 
 
-def get_name():
-    return "Jessica"
-
+def get_name(ti):
+    ti.xcom_push(key='first_name', value='Jessica')
+    ti.xcom_push(key='last_name', value='Silva')
 
 with DAG(
     default_args=default_args,
-    dag_id='dag_with_python_operator_parameters',
+    dag_id='dag_with_python_operator_parameters-xcom_first_last_name',
     description='Dag with parameters',
     start_date=datetime(2023, 7, 12),
     schedule_interval='@daily'
@@ -28,10 +30,12 @@ with DAG(
     task1 = PythonOperator(
         task_id='greet',
         python_callable=greet,
-        op_kwargs={'name': 'Jessica', 'age': 27}
+        op_kwargs={'age': 27}
     )
 
     task2 = PythonOperator(
        task_id='get_name',
        python_callable=get_name
     )
+
+    task2 >> task1
