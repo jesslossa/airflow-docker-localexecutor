284ea0dd593851311d43fe01c86368cca63f0e62
Jessica Lossavaro Silva <jessica.lossavaro@semantix.ai>
2023-07-14 12:08:28 -0300

    Added xcoms fisrt test dag

---
+++ dags/dag_python_operator_xcom.py
@@ -0,0 +1,40 @@
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+
+
+default_args = {
+    'owner': 'Jessica',
+    'retries': 5,
+    'retry_delay': timedelta(minutes=5)
+}
+
+def greet(age, ti):
+    name = ti.xcoms_pull(task_ids='get_name')
+    print(f"Hello World from {name}! I am {age} years old.")
+
+
+def get_name():
+    return "Jessica"
+
+
+with DAG(
+    default_args=default_args,
+    dag_id='dag_with_python_operator_parameters-xcom_test',
+    description='Dag with parameters - v1',
+    start_date=datetime(2023, 7, 12),
+    schedule_interval='@daily'
+) as dag:
+    task1 = PythonOperator(
+        task_id='greet',
+        python_callable=greet,
+        op_kwargs={'age': 27}
+    )
+
+    task2 = PythonOperator(
+       task_id='get_name',
+       python_callable=get_name
+    )
+
+    task2 >> task1
