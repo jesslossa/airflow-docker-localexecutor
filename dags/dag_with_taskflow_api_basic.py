1343453ea0fa3684e1bab148a9982ae60cda7638
Jessica Lossavaro Silva <jessica.lossavaro@semantix.ai>
2023-07-14 12:47:43 -0300

    Fixed syntax error dag taskflow

---
+++ dags/dag_with_taskflow_api_basic.py
@@ -0,0 +1,36 @@
+from datetime import datetime, timedelta
+
+from airflow.decorators import dag, task
+
+
+default_args = {
+    'owner': 'Jessica',
+    'retries': 5,
+    'retry_delay': timedelta(minutes=5)
+}
+
+@dag(dag_id='basic_dag_with_taskflow_api', 
+     description='Basic dag using taskflow api',
+     default_args=default_args, 
+     start_date=datetime(2023, 7, 12), 
+     schedule_interval='@daily')
+def hello_world_etl():
+
+    @task()
+    def get_name():
+        return 'Jessica'
+
+    @task()
+    def get_age():
+        return 27
+
+    @task()
+    def greet(name, age):
+        print(f"Hello World! My name is {name} "
+              f"and I am {age} years old!")
+    
+    name = get_name()
+    age = get_age()
+    greet(name=name, age=age)
+
+greet_dag = hello_world_etl()
